c73173517fe7477c14a2cee99f87bead8f930bce
Brian kimanzi <migelbrian3@gmail.com>
2026-08-27 02:06:17 +0300

    new code update

---
+++ JavaScript/index.js
@@ -0,0 +1,18 @@
+// console.log(`Hello World!`);
+// console.log(`i like pizza!`);
+
+// // cleating an alert box
+// window.alert(`i like pizza!`);
+// window.alert(`This is an Alert`);
+
+document.getElementById("myH1").textContent = `Hello People`;
+document.getElementById("myP").textContent = `I like Pizza`;
+
+// creating a comments
+
+// creating a multi-line comment
+/*
+    This is a multi-line comment
+    This is a multi-line comment
+    
+    */
