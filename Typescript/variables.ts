84027edaf3a281b414e5f621c19710e39ee8bc79
Brian kimanzi <migelbrian3@gmail.com>
2026-08-27 02:56:03 +0300

    new code version update

--- Typescript/variables.ts
+++ Typescript/variables.ts
@@ -1,3 +1,15 @@
 let x = 2; // implicit type setting 
 // let x: number = 2 // this is explicit defining the type on variable
 
+// using explicit when you dont want to assign a value
+let y: number;
+
+y = 3
+
+// string
+let z: string;
+//boolean
+let a: boolean;
+
+// null used when you want to explicitly define something as empty or non existent
+let result = null;
