6aa84e22e75a2c43d77b02e5f9180db146894782
Brian kimanzi <migelbrian3@gmail.com>
2026-08-29 05:07:03 +0300

    new code version update

--- JavaScript/typeConversion.js
+++ JavaScript/typeConversion.js
@@ -1,9 +1,20 @@
 /*Type conversion = change the datatype of a value to another 
 (String, numbers, booleans) */
 
-let age = window.prompt("How old are you? ")
-age  = Number(age) // converting the variable age from string to number
+// let age = window.prompt("How old are you? ")
+// age  = Number(age) // converting the variable age from string to number
 
-age +=1
+// age +=1
 
-console.log(age)
+// console.log(age)
+
+// Constant variables
+
+const PI = 3.142
+let radius, circumference
+
+radius = window.prompt("Enter the radius: ")
+radius = Number(radius)
+
+circumference = 2 * PI * radius
+console.log(circumference)
