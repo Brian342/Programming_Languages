e7a9c7bdc3e088292db3614d9111a589df3fcf09
Brian kimanzi <migelbrian3@gmail.com>
2026-08-29 06:44:48 +0300

    new code version update

--- Typescript/literal.ts
+++ Typescript/literal.ts
@@ -8,3 +8,23 @@ let responseCode: 200 | 404 | 201
 // enums (Enumeration) - enables developers to establish a collection of named constants (enumerators) 
 // each linked with an integer value
 
+enum size {
+    small,
+    medium,
+    large
+}
+
+var Size: size = size.small
+
+if (Size === size.small){
+
+}
+
+enum Direction {
+    up = "UP",
+    Down = "DOWN",
+    Left = "LEFT",
+    Right = "RIGHT"
+}
+
+// enums are treated as data types and you can use them to create sets of constants for use with variables and properties
