81c9b6ba263030e8920aade3d938fc88fb8a5795
Brian kimanzi <migelbrian3@gmail.com>
2026-08-29 04:35:07 +0300

    new code version update

---
+++ JavaScript/typeConversion.js
@@ -0,0 +1,9 @@
+/*Type conversion = change the datatype of a value to another 
+(String, numbers, booleans) */
+
+let age = window.prompt("How old are you? ")
+age  = Number(age) // converting the variable age from string to number
+
+age +=1
+
+console.log(age)
