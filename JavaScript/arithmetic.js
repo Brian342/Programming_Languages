e56f0eb1bea3fc4c70587ece8c553fc18c92cc2f
Brian kimanzi <migelbrian3@gmail.com>
2026-08-29 03:52:23 +0300

    new code version

--- JavaScript/arithmetic.js
+++ JavaScript/arithmetic.js
@@ -2,3 +2,23 @@
                             operators (+ - * /)
                             eg 11 = x + 5
 */
+
+let students  = 30
+
+// students += 1
+// students -= 1
+// students *= 2
+// students /= 2
+// students **= 2
+// students ++
+// students --
+
+/*Operator Precedence
+1. parenthesis ()
+2. exponents
+3. multiplication & division & modulo
+4. addition & substraction
+*/
+
+let result  = 1 + 2 * 3 - 4 ** 2
+console.log(result)
