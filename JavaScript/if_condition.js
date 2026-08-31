37f90f9f77c014d46c1b5f072f55b77b24908e3c
Brian kimanzi <migelbrian3@gmail.com>
2026-08-31 04:48:28 +0300

    new code version update

---
+++ JavaScript/if_condition.js
@@ -0,0 +1,8 @@
+/*IF Condition - if a condition is true, execute some code 
+                 if not, do something else */
+
+let age = 25
+
+if(age>= 18){
+    console.log("You are old enough to vote!")
+}
