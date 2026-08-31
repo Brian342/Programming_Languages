72e985f21e91dae987104c8213cab828e13c0677
Brian kimanzi <migelbrian3@gmail.com>
2026-08-31 06:53:27 +0300

    new code version update

--- Typescript/functions.ts
+++ Typescript/functions.ts
@@ -6,3 +6,27 @@ function add(x:number, y:number): number | string{
 }
 
 const result  = add(1, 2)
+
+
+function makeName(firstName: string, lastName: string, middleName?:string){
+    if (middleName) return firstName + " " + middleName + " " + lastName
+    return firstName + " " + lastName
+}
+
+const fullName = makeName("Tim", "James")
+
+
+
+function Names(firstName: string, lastName: string, middleName:string = "middle"){
+    if (middleName) return firstName + " " + middleName + " " + lastName
+    return firstName + " " + lastName
+}
+
+const Name = makeName("Tim", "James")
+
+function callFunc(func:(f: string, l:string, m?: string)=>string,
+ param1: string, param2:string){
+    
+
+}
+callFunc(makeName, "Tim", "James")
