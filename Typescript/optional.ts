82ba42ff1dba05ad96a0c21f07198d68567c2526
Brian kimanzi <migelbrian3@gmail.com>
2026-08-31 06:30:53 +0300

    new code version update

--- Typescript/optional.ts
+++ Typescript/optional.ts
@@ -3,3 +3,15 @@
 // and deal with undefined values withing Typescript
 
 const arr = [{name: "tim"}, {name: "joe"}, {name: "jane"}]
+
+const el = arr.pop()?.name
+// pop() removes/returns the last element from the array
+//? operator is checking and then moving forward
+
+
+// Exclamation point(!) operator tells the compiler to ignore 
+// the possibility of it being undefined it is forcing us to move forward
+const arr2 = [[{name:"tim"}]]
+const el2 = arr2.pop()!.pop()!.name
+
+
