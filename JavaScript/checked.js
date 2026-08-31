e04c686cff60e4cd8d1285deccd53158263ff161
Brian kimanzi <migelbrian3@gmail.com>
2026-09-01 01:53:07 +0300

    new code version update

--- JavaScript/checked.js
+++ JavaScript/checked.js
@@ -1,1 +1,9 @@
 //.checked = property that determines the checked state of an HTML checkbox or radio button element
+const myCheckBox = document.getElementById("myCheckBox")
+const visaBtn = document.getElementById("visaBtn")
+const masterCardBtn = document.getElementById("masterCardBtn")
+const payPalBtn = document.getElementById("payPalBtn")
+const mySubmit = document.getElementById("mySubmit")
+const subResult = document.getElementById("subResult")
+const paymentResult = document.getElementById("paymentResult")
+
