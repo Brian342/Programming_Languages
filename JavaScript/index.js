3b2e7a840a59502a0aadb12b7bd58d7f7851f820
Brian kimanzi <migelbrian3@gmail.com>
2026-08-31 04:45:49 +0300

    new code version update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,18 +1,31 @@
-// console.log(`Hello World!`);
-// console.log(`i like pizza!`);
 
 // // cleating an alert box
 // window.alert(`i like pizza!`);
 // window.alert(`This is an Alert`);
 
-document.getElementById("myH1").textContent = `Hello People`;
-document.getElementById("myP").textContent = `I like Pizza`;
+// document.getElementById("myH1").textContent = `Hello People`;
+// document.getElementById("myP").textContent = `I like Pizza`;
 
-// creating a comments
+// Random number Generator
+const myButton = document.getElementById("myButton")
+const Label1 = document.getElementById("Label1")
+const Label2 = document.getElementById("Label2")
+const Label3 = document.getElementById("Label3")
 
-// creating a multi-line comment
-/*
-    This is a multi-line comment
-    This is a multi-line comment
-    
-    */
+const min = 1
+const max = 6
+
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+
+myButton.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    Label1.textContent = randomNum1
+    Label2.textContent = randomNum2
+    Label3.textContent = randomNum3
+
+}
