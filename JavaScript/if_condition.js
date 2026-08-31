d14e99f1b5f8f5f728cf1304b7d365518488b231
Brian kimanzi <migelbrian3@gmail.com>
2026-08-31 05:43:01 +0300

    new code version update

--- JavaScript/if_condition.js
+++ JavaScript/if_condition.js
@@ -1,8 +1,16 @@
 /*IF Condition - if a condition is true, execute some code 
                  if not, do something else */
 
-let age = 25
+let age = 19
+let hasLicence = false
 
 if(age>= 18){
-    console.log("You are old enough to vote!")
+    console.log("You are old enough to drive")
+    if(hasLicence){
+        console.log("You have your licence")
+    }else{
+        console.log("You dont have a licence")
+    }
+}else{
+    console.log("You must be 18+ to have a licence!!")
 }
---
+++ Typescript/Type_cast.ts
@@ -0,0 +1,23 @@
+// any type allows for flexibility typing 
+// but sacrifices type safety as it lacks compile time type checking
+
+// you use "any type" typically when you're in a very complex 
+// situation and you're not able to predict what the type of 
+// the variables gonna be
+
+
+let x: any = 1
+
+x.lenth
+
+// The unknown type is a type-safe counterpart to the any type
+// Unknown type provides a powerful way to handle values of uncertain types
+// while maintaining type safety
+let y: unknown = 1;
+
+if (typeof y == "number"){
+    const result = x + 1;
+} else if (typeof y == "string"){
+    const result = y.length
+}
+
